569a277f2ce84de55e247a9e5687e906ca01de5b
Giovanni Román-Díaz <28658784+gioromandiaz@users.noreply.github.com>
2023-03-08 17:55:16 -0500

    Update insert organization.sql

--- insert organization.sql
+++ insert organization.sql
@@ -2,28 +2,37 @@
 	Script for adding organizations into the DB
 */
 
+/*DYNAMIC*/
 DECLARE @Location_ID INT;
 DECLARE @Phone_ID INT;
 DECLARE @Email_ID INT;
 
+/*CONST*/
 DECLARE @Organization_Type_ID INT;
 SET @Organization_Type_ID = 2;
 
 DECLARE @Phone_Type_ID INT;
 SET @Phone_Type_ID = 1;
 
+DECLARE @Country VARCHAR;
+SET @Country = 'USA';
+
+DECLARE @Email VARCHAR;
+SET @Email = 'N/A';
+
+-- Insert address into Location table and get its ID
 INSERT INTO Location (Street_Number, Street_Name, City, State, Zip_Code, Country, County)
-VALUES ('424', 'East Central Boulevard, Suite 542', 'Orlando', 'FL', '32801', 'USA', 'Orange County');
+VALUES ('270', 'W. PLANT ST. SUITE 210', 'WINTER GARDEN', 'FL', '34787', @Country, 'Orange County');
 SET @Location_ID = SCOPE_IDENTITY();
 
 -- Insert email into Phone table and get its ID 
 INSERT INTO Phone (Phone_Number, Phone_Type_ID) 
-VALUES ('407-416-9960', @Phone_Type_ID);
+VALUES ('217-540-2191', @Phone_Type_ID);
 SET @Phone_ID = SCOPE_IDENTITY(); 
 
 -- Insert email into Email table and get its ID 
 INSERT INTO Email (Email_Address) 
-VALUES (NULL);
+VALUES ('info@wmgdevelopment.com');
 SET @Email_ID = SCOPE_IDENTITY();
 
 -- Declare variables for Organization_ID to be used to push in with project data
@@ -31,4 +40,4 @@ DECLARE @Organization_ID INT;
 
 -- Insert Company data into Organization table and get its IDfor the Project Insert link 
 INSERT INTO Organization (Name, Location_ID, Phone_ID, Email_ID, Organization_Type_ID) 
-VALUES ('Eleven 18 Architecture LLC', @Location_ID, @Phone_ID, @Email_ID, @Organization_Type_ID);
+VALUES ('WMG Development', @Location_ID, @Phone_ID, @Email_ID, @Organization_Type_ID);
